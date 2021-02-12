8dd6fea19fbb370781c8c66db346de0ef63bbe37
Alejandro Esteban Celdrán <aestebance@uoc.edu>
2021-02-12 18:43:04 +0100

    initial commit

--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,26 @@
 const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
-  
+
   const translator = new Translator();
 
   app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+      .post((req, res) => {
+        if (!req.body.hasOwnProperty('text') ||
+            !(req.body.locale === "british-to-american" || req.body.locale === "american-to-british")) {
+          return res.json({
+            error: 'Required field(s) missing'
+          });
+        }
+        if (req.body.text === "") {
+          return res.json({
+            error: 'No text to translate'
+          });
+        }
+        const {text, locale} = req.body;
+
+        return res.json({
+          result: "all correct"
+        });
+      });
 };
