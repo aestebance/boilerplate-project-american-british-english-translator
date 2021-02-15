f1a5ee33e6272c36399fbe7a40c16c99d494aaba
Alejandro Esteban Celdrán <aestebance@uoc.edu>
2021-02-15 20:50:42 +0100

    finishing fixing errors

--- components/translator.js
+++ components/translator.js
@@ -27,7 +27,6 @@ class Translator {
 
     translate(text, mode) {
         let newString = text;
-        newString = newString.charAt(0).toUpperCase() + newString.slice(1);
         let spanOpen = "<span class='highlight'>";
         let spanClose = "</span>";
         if (mode === 'american-to-british') {
@@ -63,6 +62,7 @@ class Translator {
                 }
             })
         }
+        newString = newString.charAt(0).toUpperCase() + newString.slice(1);
         if (text === newString) {
             return 'Everything looks good to me!';
         } else {
